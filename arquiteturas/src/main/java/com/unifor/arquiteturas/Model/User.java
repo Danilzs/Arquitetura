2cb7b2c95448757477c00dfe119267bdea9ffbf2
Danilzs <danzone00@gmail.com>
2025-09-07 22:42:50 -0300

    Correção do user e userRepository

--- arquiteturas/src/main/java/com/unifor/arquiteturas/Model/User.java
+++ arquiteturas/src/main/java/com/unifor/arquiteturas/Model/User.java
@@ -1,13 +1,19 @@
 package com.unifor.arquiteturas.Model;
 
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
-
+@Entity
 public class User {
-    private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String name;
     private String email;
-
 }
--- arquiteturas/src/main/java/com/unifor/arquiteturas/Repository/UserRepository.java
+++ arquiteturas/src/main/java/com/unifor/arquiteturas/Repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.unifor.arquiteturas.Repository;
 
 import com.unifor.arquiteturas.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, long> {
-
+public interface UserRepository extends JpaRepository<User, Long> {
 }
